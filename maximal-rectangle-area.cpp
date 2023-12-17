131cc8ad8b25237155feb67b727ebf5fc028d43f
Rakesh Meena <rakeshmeena5499@gmail.com>
2023-12-17 12:40:48 +0530

    Stack Questions

---
+++ maximal-rectangle-area.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int maxAreaHistogram(vector<int>& heights){
+        int n = heights.size();
+        stack<int> st;
+        int left[n], right[n];
+        for(int i=0;i<n;i++){
+            while(!st.empty() && heights[st.top()] >= heights[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                left[i] = 0;
+            }
+            else{
+                left[i] = st.top() + 1;
+            }
+            st.push(i);
+        }  
+        while(!st.empty()){
+            st.pop();
+        }
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && heights[st.top()] >= heights[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                right[i] = n-1;
+            }
+            else{
+                right[i] = st.top() - 1;
+            }
+            st.push(i);
+        }
+        int maxArea = 0;
+        for(int i=0;i<n;i++){
+            maxArea = max(maxArea, heights[i] * (right[i] - left[i] + 1));
+        }
+        return maxArea;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int maxArea = 0;
+        vector<int> height(m, 0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j] == '1'){
+                    height[j] += 1;
+                }
+                else{
+                    height[j] = 0;
+                }
+            }
+            int area = maxAreaHistogram(height);
+            maxArea = max(maxArea, area);
+        }
+        return maxArea;
+    }
+};

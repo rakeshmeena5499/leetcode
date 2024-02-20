4debc8fc51b7c9484a23ba2c03d3fb9ded1d1861
Rakesh Meena <rakeshmeena5499@gmail.com>
2024-02-20 20:52:21 +0530

    Bit Manipulation Questions

--- missing-number.cpp
+++ missing-number.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        // int sum = accumulate(nums.begin(), nums.end(), 0);
+        // int n = nums.size();
+        // return n*(n+1)/2 - sum;
         int numXor = 0, trueXor = 0;
         for(int i=0;i<nums.size();i++){
             numXor ^= nums[i];

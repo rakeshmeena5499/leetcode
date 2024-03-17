1c0fb413224c8dd927c56501926461ae39c8d254
Rakesh Meena <rakeshmeena5499@gmail.com>
2024-03-17 09:51:16 +0530

    Interval Questions

--- insert-intervals.cpp
+++ insert-intervals.cpp
@@ -1,15 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-        sort(intervals.begin(), intervals.end());
+        // intervals.push_back(newInterval);
+        // sort(intervals.begin(), intervals.end());
+        // vector<vector<int>> res;
+        // res.push_back(intervals[0]);
+        // for(int i=1;i<intervals.size();i++){
+        //     if(res.back()[1]>=intervals[i][0])
+        //         res.back()[1] = max(res.back()[1], intervals[i][1]);
+        //     else
+        //         res.push_back(intervals[i]);
+        // }
+        // return res;
+
         vector<vector<int>> res;
-        res.push_back(intervals[0]);
-        for(int i=1;i<intervals.size();i++){
-            if(res.back()[1]>=intervals[i][0])
-                res.back()[1] = max(res.back()[1], intervals[i][1]);
-            else
-                res.push_back(intervals[i]);
+        int i = 0;
+        int n = intervals.size();
+
+        // Case 1: no overlapping from front
+        while(i<n && intervals[i][1] < newInterval[0]){
+            res.push_back(intervals[i]);
+            i++;
+        }
+
+        // Case 2 : Merge overlapping intervals
+        while(i<n && newInterval[1] >= intervals[i][0]){
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        res.push_back(newInterval);
+
+        //Case 3 : no overlapping after merging
+        while(i<n){
+            res.push_back(intervals[i++]);
         }
         return res;
     }

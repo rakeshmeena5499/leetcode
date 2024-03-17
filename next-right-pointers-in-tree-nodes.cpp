74741239e74004512afca94038b39dfed92903e9
Rakesh Meena <rakeshmeena5499@gmail.com>
2024-03-17 18:15:04 +0530

    Tree Questions

---
+++ next-right-pointers-in-tree-nodes.cpp
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    void solve(Node* root){
+        if(root == NULL || root->left == NULL){
+            return;
+        }
+        root->left->next = root->right;
+        if(root->next!=NULL){
+            root->right->next = root->next->left;
+        }
+        solve(root->left);
+        solve(root->right);
+    }
+
+    Node* connect(Node* root) {
+        solve(root);
+        return root;
+    }
+};

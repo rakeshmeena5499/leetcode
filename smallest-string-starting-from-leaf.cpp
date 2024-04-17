58d9b6d2c2a845cd4e48a6a853d6656ee1e6b85c
Rakesh Meena <rakeshmeena5499@gmail.com>
2024-04-17 14:52:07 +0530

    Tree Questions

---
+++ smallest-string-starting-from-leaf.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string ans;
+    void solve(TreeNode* root, string curr){
+        if(root == NULL){
+            return;
+        }
+        curr = char(root->val + 'a') + curr;
+        if(root->left == NULL && root->right == NULL){
+            if(ans == "" || ans > curr){
+                ans = curr;
+            }
+        }
+        solve(root->left, curr);
+        solve(root->right, curr);
+    }
+
+    string smallestFromLeaf(TreeNode* root) {
+        ans = "";
+        string curr  = "";
+        solve(root, curr);
+        return ans;
+    }
+};
